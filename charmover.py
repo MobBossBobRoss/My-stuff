cb22b3ee7bfe6a1d720510df21bae753444184a2
Jacob <53247473+MobBossBobRoss@users.noreply.github.com>
2019-08-23 23:48:18 -0500

    Updated char_mover to be usable with other computers/OSes

--- charmover.py
+++ charmover.py
@@ -1,16 +1,35 @@
 #! python3
-# charmover.py - This program extracts/moves files from my mugen character
-# and stage download folders to my mugen chars/stages folder.
+# charmover.py - This program extracts/moves files from its mugen character
+# and stage download folders to your mugen chars/stages folder.
 
-import os, subprocess, patoolib, zipfile, re
+import os
+import patoolib
+import re
 
 
 def charmover():
-    downloaddir = 'C:\\users\\jtoat\\downloads\\MUGEN stuff\\'
-    chardir = downloaddir + 'MUGEN chars\\'
+    downloaddir = os.getcwd()
+    if not os.path.exists(os.path.join(downloaddir, 'Mugen chars')):
+        os.mkdir(os.path.join(downloaddir, 'MUGEN chars'))
+    chardir = os.path.join(downloaddir, 'MUGEN chars')
     charfiles = os.listdir(chardir)
-    chardest = 'C:\\Games\\M.I.C.A Final Version Edition 1.1b1\\chars\\'
-    extractdir = downloaddir + 'Extracted_Files\\'
+    if os.path.exists('./path.txt'):
+        with open('./path.txt', 'r') as m:
+            mugenroot = m.read()
+    else:
+        while True:
+            print('Please input the path to your MUGEN root folder.')
+            mugenroot = input('> ')
+            if os.path.exists(os.path.join(mugenroot, 'chars')):
+                with open('./path.txt', 'w') as m:
+                    m.write(mugenroot)
+                break
+            else:
+                print('It seems that directory is invalid.')
+    chardest = os.path.join(mugenroot, 'chars')
+    if not os.path.exists(os.path.join(downloaddir, 'Extracted Files')):
+        os.mkdir(os.path.join(downloaddir, 'Extracted Files'))
+    extractdir = os.path.join(downloaddir, 'Extracted Files')
     tally = 0
     for char in charfiles:
         pathtochar = os.path.join(chardir, char)
@@ -19,35 +38,50 @@ def charmover():
             if len(os.listdir(extractdir)) == 1:
                 tally += 1
                 for file in os.listdir(extractdir):
-                    os.rename(extractdir + file, chardest + file)
+                    os.rename(os.path.join(extractdir, file), os.path.join(chardest, file))
             else:
                 if char[-3:] == '.7z':
-                    os.mkdir(chardir + char[0:-3])
+                    os.mkdir(os.path.join(chardir, char[:-3]))
                     newdirname = char[:-3]
-                    newdir = chardir + newdirname + '\\'
+                    newdir = os.path.join(newdir, newdirname)
                 else:
-                    os.mkdir(chardir + char[:-4])
+                    os.mkdir(os.path.join(chardir, char[:-4]))
                     newdirname = char[:-4]
-                    newdir = chardir + newdirname + '\\'
+                    newdir = os.path.join(newdir, newdirname)
                 for file in os.listdir(extractdir):
-                    os.rename(extractdir + file, newdir + file)
-                os.rename(newdir, chardest + newdirname)
+                    os.rename(os.path.join(extractdir, file), os.path.join(newdir, file))
+                os.rename(newdir, os.path.join(chardest, newdirname))
                 tally += 1
             os.remove(pathtochar)
         else:
             tally += 1
-            os.rename(pathtochar, chardest + char)
-    print('\nCharacter transfer complete. {} characters were moved.'.format(tally))
+            os.rename(pathtochar, os.path.join(chardest, char))
+    print(f'\nCharacter transfer complete. {tally} characters were moved.')
 
 
 def stagemover():
-    downloaddir = 'C:\\users\\jtoat\\downloads\\MUGEN stuff\\'
-    stagedir = downloaddir + 'Mugen stages\\'
+    downloaddir = os.getcwd()
+    if not os.path.exists(os.path.join(downloaddir, 'Mugen stages')):
+        os.mkdir(os.path.join(downloaddir, 'MUGEN stages'))
+    stagedir = os.path.join(downloaddir, 'MUGEN stages')
     stagefiles = os.listdir(stagedir)
-    stagedest = 'C:\\Games\\M.I.C.A Final Version Edition 1.1b1\\stages\\'
-    sounddest = 'C:\\Games\\M.I.C.A Final Version Edition 1.1b1\\sound\\'
+    if os.path.exists('./path.txt'):
+        with open('./path.txt', 'r') as m:
+            mugenroot = m.read()
+    else:
+        while True:
+            print('Please input the path to your MUGEN root folder.')
+            mugenroot = input('> ')
+            if os.path.exists(os.path.join(mugenroot, 'stages')):
+                with open('./path.txt', 'w') as m:
+                    m.write(mugenroot)
+                break
+            else:
+                print('It seems that directory is invalid.')
+    stagedest = os.path.join(mugenroot, 'stages')
+    sounddest = os.path.join(mugenroot, 'sound')
     stagecontents = os.listdir(stagedest)
-    includedsoundcontents = os.listdir(stagedest + 'sound')
+    includedsoundcontents = os.listdir(os.path.join(stagedest, 'sound'))
     musicRegex = re.compile(r'.*\.(?:ogg|mp3)')
     tally = 0
     for stage in stagefiles:
@@ -64,9 +98,20 @@ def stagemover():
     for item in includedsoundcontents:
         if musicRegex.findall(str(item)) != []:
             os.rename(os.path.join(stagedest + 'sound\\', item), sounddest + str(item))
-    print('Stage transfer complete. {} stages were moved.'.format(tally))
+    print(f'Stage transfer complete. {tally} stages were moved.\n')
+    while True:
+        print('Would you like to run VSelect? y/n')
+        r = input('> ')
+        if r.lower() == 'y':
+            if os.path.exists(os.path.join(mugenroot, 'VSelect.exe')):
+                os.startfile(os.path.join(mugenroot, 'VSelect.exe'))
+                break
+            else:
+                print('VSelect could not be found. Please make sure that VSelect.exe is inside your root MUGEN folder.')
+                input('> ')
+                break
 
 
 charmover()
 stagemover()
-os.startfile(r'C:\Games\M.I.C.A Final Version Edition 1.1b1\VSelect.exe')
+
