2691ea81d62d66a96507136cb89f6e9c165cd476
Jacob <53247473+MobBossBobRoss@users.noreply.github.com>
2019-08-24 23:20:34 -0500

    More tweaks to cleanliness and usability

--- charmover.py
+++ charmover.py
@@ -13,15 +13,15 @@ def charmover():
         os.mkdir(os.path.join(downloaddir, 'MUGEN chars'))
     chardir = os.path.join(downloaddir, 'MUGEN chars')
     charfiles = os.listdir(chardir)
-    if os.path.exists('./path.txt'):
-        with open('./path.txt', 'r') as m:
+    if os.path.exists(os.path.join(os.getcwd(), 'path.txt')):
+        with open(os.path.join(os.getcwd(), 'path.txt')) as m:
             mugenroot = m.read()
     else:
         while True:
             print('Please input the path to your MUGEN root folder.')
             mugenroot = input('> ')
             if os.path.exists(os.path.join(mugenroot, 'chars')):
-                with open('./path.txt', 'w') as m:
+                with open(os.path.join(os.getcwd(), 'path.txt')) as m:
                     m.write(mugenroot)
                 break
             else:
@@ -65,15 +65,15 @@ def stagemover():
         os.mkdir(os.path.join(downloaddir, 'MUGEN stages'))
     stagedir = os.path.join(downloaddir, 'MUGEN stages')
     stagefiles = os.listdir(stagedir)
-    if os.path.exists('./path.txt'):
-        with open('./path.txt', 'r') as m:
+    if os.path.exists(os.path.join(os.getcwd(), 'path.txt')):
+        with open(os.path.join(os.getcwd(), 'path.txt')) as m:
             mugenroot = m.read()
     else:
         while True:
             print('Please input the path to your MUGEN root folder.')
             mugenroot = input('> ')
             if os.path.exists(os.path.join(mugenroot, 'stages')):
-                with open('./path.txt', 'w') as m:
+                with open(os.path.join(os.getcwd(), 'path.txt')) as m:
                     m.write(mugenroot)
                 break
             else:
@@ -81,7 +81,7 @@ def stagemover():
     stagedest = os.path.join(mugenroot, 'stages')
     sounddest = os.path.join(mugenroot, 'sound')
     stagecontents = os.listdir(stagedest)
-    includedsoundcontents = os.listdir(os.path.join(stagedest, 'sound'))
+    includedsound = os.path.join(stagedir, 'sound')
     musicRegex = re.compile(r'.*\.(?:ogg|mp3)')
     tally = 0
     for stage in stagefiles:
@@ -89,16 +89,18 @@ def stagemover():
         pathtostage = os.path.join(stagedir, stage)
         if os.path.isfile(pathtostage):
             patoolib.extract_archive(pathtostage, outdir=stagedest)
-        else:
-            os.rename(pathtostage, stagedest + stage)
             os.remove(pathtostage)
+        else:
+            os.rename(pathtostage, os.path.join(stagedest, stage))
+        if os.path.exists(includedsound):
+            includedsoundcontents = os.listdir(includedsound)
+            for item in includedsoundcontents:
+                os.rename(os.path.join(includedsound, item), os.path.join(sounddest, item))
+            os.rmdir(includedsound)
     for item in stagecontents:
         if musicRegex.findall(str(item)) != []:
-            os.rename(os.path.join(stagedest, item), sounddest + str(item))
-    for item in includedsoundcontents:
-        if musicRegex.findall(str(item)) != []:
-            os.rename(os.path.join(stagedest + 'sound\\', item), sounddest + str(item))
-    print(f'Stage transfer complete. {tally} stages were moved.\n')
+            os.rename(os.path.join(stagedest, item), os.path.join(sounddest, item))
+        print(f'Stage transfer complete. {tally} stages were moved.\n')
     while True:
         print('Would you like to run VSelect? y/n')
         r = input('> ')
@@ -110,6 +112,8 @@ def stagemover():
                 print('VSelect could not be found. Please make sure that VSelect.exe is inside your root MUGEN folder.')
                 input('> ')
                 break
+        if r.lower() == 'n':
+            break
 
 
 charmover()
